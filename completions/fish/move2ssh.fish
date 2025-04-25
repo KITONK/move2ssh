4ce19302a5cc4773c6549f17f5417d8f0a026d04
KITO <kiyj585@gmail.com>
2025-04-25 17:11:07 +0300

    fix:  autocomplete

--- completions/fish/move2ssh.fish
+++ completions/fish/move2ssh.fish
@@ -1,10 +1,19 @@
+# Define commands
 complete -c bin/move2ssh -f
+complete -c move2ssh -f
 
-# Folders completion
-complete -c bin/move2ssh -n "__fish_is_first_arg" -a "(find $HOME/.ssh -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
+# Folders completion for both variants
+complete -c bin/move2ssh -n "not __fish_seen_argument -l help -l h -l list -l l -l move -l m -l backup -l b" -a "(find $HOME/.ssh -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
+complete -c move2ssh -n "not __fish_seen_argument -l help -l h -l list -l l -l move -l m -l backup -l b" -a "(find $HOME/.ssh -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
 
-# Options
-complete -c bin/move2ssh -l help -s h -d 'Show help message'
-complete -c bin/move2ssh -l list -s l -d 'List all directories'
-complete -c bin/move2ssh -l move -s m -d 'Move existing files'
-complete -c bin/move2ssh -l backup -s b -d 'Create backup'
+# Options for bin/move2ssh
+complete -c bin/move2ssh -n 'not __fish_seen_argument -l help -l h' -l help -s h -d 'Show help message'
+complete -c bin/move2ssh -n 'not __fish_seen_argument -l list -l l' -l list -s l -d 'List all directories'
+complete -c bin/move2ssh -n 'not __fish_seen_argument -l move -l m' -l move -s m -d 'Move existing files'
+complete -c bin/move2ssh -n 'not __fish_seen_argument -l backup -l b' -l backup -s b -d 'Create backup'
+
+# Options for move2ssh
+complete -c move2ssh -n 'not __fish_seen_argument -l help -l h' -l help -s h -d 'Show help message'
+complete -c move2ssh -n 'not __fish_seen_argument -l list -l l' -l list -s l -d 'List all directories'
+complete -c move2ssh -n 'not __fish_seen_argument -l move -l m' -l move -s m -d 'Move existing files'
+complete -c move2ssh -n 'not __fish_seen_argument -l backup -l b' -l backup -s b -d 'Create backup'
--- completions/powershell/move2ssh.ps1
+++ completions/powershell/move2ssh.ps1
@@ -1,16 +1,48 @@
-Register-ArgumentCompleter -Native -CommandName bin/move2ssh -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-    
-    $folders = Get-ChildItem "$env:USERPROFILE\.ssh" -Directory | Select-Object -ExpandProperty Name
-    $options = @('--help', '--h', '--list', '--l', '--move', '--m', '--backup', '--b')
-    
-    if ($wordToComplete -match '^-') {
-        $options | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+@('bin/move2ssh', 'move2ssh') | ForEach-Object {
+    Register-ArgumentCompleter -Native -CommandName $_ -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+        
+        # Define the path to .ssh depending on the environment
+        $sshPath = if ($env:WSLENV) {
+            # For WSL
+            "$env:USERPROFILE\.ssh"
+        } else {
+            # For regular PowerShell
+            "$env:USERPROFILE\.ssh"
         }
-    } else {
-        $folders | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        
+        $folders = Get-ChildItem $sshPath -Directory | Select-Object -ExpandProperty Name
+        $options = @(
+            @{Name='--help'; Description='Show help message'},
+            @{Name='--h'; Description='Show help message'},
+            @{Name='--list'; Description='List all directories'},
+            @{Name='--l'; Description='List all directories'},
+            @{Name='--move'; Description='Move existing files'},
+            @{Name='--m'; Description='Move existing files'},
+            @{Name='--backup'; Description='Create backup'},
+            @{Name='--b'; Description='Create backup'}
+        )
+        
+        if ($wordToComplete -match '^-') {
+            $options | ForEach-Object {
+                if ($_.Name -like "$wordToComplete*") {
+                    [System.Management.Automation.CompletionResult]::new(
+                        $_.Name,
+                        $_.Name,
+                        'ParameterValue',
+                        $_.Description
+                    )
+                }
+            }
+        } else {
+            $folders | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new(
+                    $_,
+                    $_,
+                    'ParameterValue',
+                    "SSH configuration folder: $_"
+                )
+            }
         }
     }
 }
